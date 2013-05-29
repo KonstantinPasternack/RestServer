0a318e1ddc80a14dbc3ec461d7a80e36175f2a8e
Michael Marschall <michael.a.m1990@gmail.com>
2013-05-29 13:42:45 +0200

    Auf ein Neues...

---
+++ GiftBoxRESTServer/src/main/java/de/giftbox/domain_ejb/BewertungenHome.java
@@ -0,0 +1,69 @@
+package de.giftbox.domain_ejb;
+
+// Generated 29.05.2013 13:40:32 by Hibernate Tools 4.0.0
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Bewertungen.
+ * @see de.giftbox.domain_ejb.Bewertungen
+ * @author Hibernate Tools
+ */
+@Stateless
+public class BewertungenHome {
+
+	private static final Log log = LogFactory.getLog(BewertungenHome.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Bewertungen transientInstance) {
+		log.debug("persisting Bewertungen instance");
+		try {
+			entityManager.persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Bewertungen persistentInstance) {
+		log.debug("removing Bewertungen instance");
+		try {
+			entityManager.remove(persistentInstance);
+			log.debug("remove successful");
+		} catch (RuntimeException re) {
+			log.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Bewertungen merge(Bewertungen detachedInstance) {
+		log.debug("merging Bewertungen instance");
+		try {
+			Bewertungen result = entityManager.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Bewertungen findById(Integer id) {
+		log.debug("getting Bewertungen instance with id: " + id);
+		try {
+			Bewertungen instance = entityManager.find(Bewertungen.class, id);
+			log.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+}
---
+++ GiftBoxRESTServer/src/main/java/de/giftbox/domain_ejb/GeschenkHome.java
@@ -0,0 +1,69 @@
+package de.giftbox.domain_ejb;
+
+// Generated 29.05.2013 13:40:32 by Hibernate Tools 4.0.0
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Geschenk.
+ * @see de.giftbox.domain_ejb.Geschenk
+ * @author Hibernate Tools
+ */
+@Stateless
+public class GeschenkHome {
+
+	private static final Log log = LogFactory.getLog(GeschenkHome.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(Geschenk transientInstance) {
+		log.debug("persisting Geschenk instance");
+		try {
+			entityManager.persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void remove(Geschenk persistentInstance) {
+		log.debug("removing Geschenk instance");
+		try {
+			entityManager.remove(persistentInstance);
+			log.debug("remove successful");
+		} catch (RuntimeException re) {
+			log.error("remove failed", re);
+			throw re;
+		}
+	}
+
+	public Geschenk merge(Geschenk detachedInstance) {
+		log.debug("merging Geschenk instance");
+		try {
+			Geschenk result = entityManager.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Geschenk findById(Integer id) {
+		log.debug("getting Geschenk instance with id: " + id);
+		try {
+			Geschenk instance = entityManager.find(Geschenk.class, id);
+			log.debug("get successful");
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+}
