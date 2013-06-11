338d2bc1bcb948a331788f8bb920896132d8b103
KonstantinPasternack <konstantin.pasternack@googlemail.com>
2013-06-11 14:56:27 +0200

    post benutzer test

--- GiftBoxRESTServer/src/main/java/de/giftbox/controller/BenutzerController.java
+++ GiftBoxRESTServer/src/main/java/de/giftbox/controller/BenutzerController.java
@@ -5,14 +5,22 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.giftbox.dao.BenutzerDAO;
 import de.giftbox.domain.Benutzer;
 
+@Controller
+@RequestMapping("/benutzer/*")
 public class BenutzerController {
+	
+	BenutzerDAO benutzerDao;
+	
 	private static final Logger log = LoggerFactory
 			.getLogger(MainController.class);
 
@@ -27,6 +35,23 @@ public class BenutzerController {
 	String postBenutzerJson(@RequestBody Benutzer benutzer) {
 		log.debug("Benutzer retrieved");
 		return "\"OK\"";
+	}
+	
+	@RequestMapping(value = "testpost/{name}", method = RequestMethod.GET)
+	public @ResponseBody String testPost(@PathVariable(value="name") String name){
+		log.info("testing Post to DB");
+		Boolean geklappt = false;
+		Benutzer benutzer = new Benutzer(name);
+		try{
+		benutzerDao.saveBenutzer(benutzer);
+		} catch (Exception ex){
+			ex.printStackTrace();
+			geklappt = false;
+		}
+		geklappt = true;
+		
+		return geklappt.toString();
+		
 	}
 
 	@RequestMapping(value = "fetchbenutzer", method = RequestMethod.GET, headers = "Accept=application/json")
