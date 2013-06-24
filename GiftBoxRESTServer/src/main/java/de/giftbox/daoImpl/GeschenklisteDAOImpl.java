2c892545e2537c0759138f2c4a599db963a37f02
chuzzla <e_realize@yahoo.de>
2013-06-24 19:20:56 +0200

    alle Geschenkliste in DAO

--- GiftBoxRESTServer/src/main/java/de/giftbox/daoImpl/GeschenklisteDAOImpl.java
+++ GiftBoxRESTServer/src/main/java/de/giftbox/daoImpl/GeschenklisteDAOImpl.java
@@ -2,15 +2,25 @@ package de.giftbox.daoImpl;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.giftbox.dao.GeschenklisteDAO;
+import de.giftbox.domain.Geschenk;
 import de.giftbox.domain.Geschenkliste;
 
+@Repository
 public class GeschenklisteDAOImpl implements GeschenklisteDAO {
 
 	private HibernateTemplate hibernateTemplate;
+	
+	@Autowired
+	SessionFactory sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.hibernateTemplate = new HibernateTemplate(sessionFactory);
@@ -27,8 +37,10 @@ public class GeschenklisteDAOImpl implements GeschenklisteDAO {
 	}
 
 	@Override
+	@Transactional
 	@SuppressWarnings("unchecked")
 	public List<Geschenkliste> listGeschenkliste() {
-		return hibernateTemplate.find("from Geschenkliste");
-	}
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(Geschenkliste.class);
+		return criteria.list();	}
 }
